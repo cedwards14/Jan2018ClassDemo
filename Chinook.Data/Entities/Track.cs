9d10cc493c5ebb67d6d8d476e6386b7698225973
cedwards14 <35229175+cedwards14@users.noreply.github.com>
2018-01-17 17:39:20 -0700

    added track class

--- Chinook.Data/Entities/Track.cs
+++ Chinook.Data/Entities/Track.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#region additional name spaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
 
 namespace Chinook.Data.Entities
 {
-    class Track
+    [Table("Tracks")]
+    public class Track
     {
+        [Key]
+        public int TrackId { get; set; }
+       [StringLength(200, ErrorMessage ="The name has a max of 200 characters")]
+       public string name { get; set; }
+        public int? AlbumId { get; set; }
+        public int MediatypeId { get; set; }
+        public int? GenreId { get; set; }
+        [StringLength(220, ErrorMessage = "The Composer has a max of 200 characters")]
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
     }
 }
